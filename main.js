1829f806441892014bc3fe33683d30871b4a5744
SofyaMogutova <78640546+SofyaMogutova@users.noreply.github.com>
2021-10-20 17:14:59 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,89 +1,204 @@
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
+let res = 0;
+count = 0;
 
-function setCookie(name, value, options = {}) {
-
-    options = {
-      path: '/',
-      ...options
-    };
-  
-    if (options.expires instanceof Date) {
-      options.expires = options.expires.toUTCString();
+function calculateAverage(){
+    count = 0;
+    res = 0;
+    document.querySelectorAll(".exercise10 .task1 .table input").forEach((item) => {
+        count++;
+        if(item.checked){
+            res += Number(item.value);
+        }
+    })
+    document.querySelector("#resSpan").innerHTML = "Cредний балл: " + res / (count  / 3);
+    if ((res / (count  / 3))<1){
+        document.querySelector("#resSpan").innerHTML = "Cредний балл: " + res / (count  / 3) + "Ты отчислен урод!!!";
     }
-  
-    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-  
-    for (let optionKey in options) {
-      updatedCookie += "; " + optionKey;
-      let optionValue = options[optionKey];
-      if (optionValue !== true) {
-        updatedCookie += "=" + optionValue;
-      }
+    calculateNumbers();
+}
+
+function updateListeners(){
+    document.querySelectorAll(".exercise10 .task1 .table input").forEach((elem) => {
+        elem.addEventListener('change', calculateAverage);
+    })
+}
+
+function getXMLDocument(url)  
+{  
+    let xml;  
+    if(window.XMLHttpRequest)  
+    {  
+        xml=new window.XMLHttpRequest();  
+        xml.open("GET", url, false);  
+        xml.send("");  
+        return xml.responseXML;  
+    }  
+    else  
+        if(window.ActiveXObject)  
+        {  
+            xml=new ActiveXObject("Microsoft.XMLDOM");  
+            xml.async=false;  
+            xml.load(url);  
+            return xml;  
+        }  
+        else  
+        {  
+            alert("Загрузка XML не поддерживается браузером");  
+            return null;  
+        }  
+} 
+
+let facultetInput = document.querySelector('#XMLfacultet');
+let courceInput = document.querySelector('#XMLcource');
+let groupSelect = document.querySelector('#XMLgroup');
+let nameSelect = document.querySelector('#XMLname');
+let xml = getXMLDocument("source/students.xml");
+let groups = xml.getElementsByTagName('group');
+let table = document.querySelector('.exercise10 .task1 .table');
+
+function fillGroups(){
+    
+    for(let i = 0; i < groups.length; i++){
+        let option = document.createElement('option');
+        let group = groups[i].getElementsByTagName('name')[0].innerHTML;
+        option.innerHTML = group;
+        option.value = group;
+        groupSelect.append(option);
     }
-  
-    document.cookie = updatedCookie;
 }
 
-const welcomeText = document.querySelector('#welcomeText');
-const countText = document.querySelector('#countText');
-const dateText = document.querySelector('#dateText');
-const nameCookie = document.querySelector('#nameCookie');
-const registrationButton = document.querySelector('#registrationButton');
-const clearButton = document.querySelector('#clearButton');
-const exitButton = document.querySelector('#exitButton');
-
-if(getCookie('name') === undefined){
-    welcomeText.innerHTML = "Зарегистрируйтесь";
-}
-else{
-    welcomeText.innerHTML = "Привет, " + getCookie('name');
-}
-
-let count = getCookie('count');
-if(count === undefined){
-    count = 0
-}
-
-setCookie('count', ++count);
-countText.innerHTML = "Вы были на этой странице: " + getCookie('count') + " раз";
-
-if(getCookie('date') === undefined){
-    setCookie('date', new Date().getTime());
-    dateText.value = "";
-}
-else{
-    let currentDate = new Date();
-    let previousDate = new Date(Number(getCookie('date')));
-    let delay = currentDate - previousDate;
-    let months = Math.floor(delay / (30 * 24 * 60 * 60 * 1000));
-    let days = Math.floor(delay / (24 * 60 * 60 * 1000)) % 30;
-    let hours = Math.floor(delay / (60 * 60 * 1000)) % 24;
-    let minutes = Math.floor(delay / (60 * 1000)) % 60;
-    let secunds = Math.floor(delay / 1000) % 60;
-    dateText.innerHTML = "Вы заходили на эту страницу: " + months  + " месяцев " + days + " дней " + hours + " часов " + minutes + " минут " + secunds + " секунд назад";
-    setCookie('date', new Date().getTime());
-}
-
-registrationButton.addEventListener('click', (event) => {
-    setCookie('name', nameCookie.value);
-    if (getCookie('name') === "Иван"){
-        welcomeText.innerHTML = "Иван в бане, досвидания ¯  (ツ)  ¯";
-    } 
-    else{
-        welcomeText.innerHTML = "Привет, " + getCookie('name');
+function fillNames(group){
+    while(nameSelect.firstChild){
+        nameSelect.firstChild.remove();
     }
+    for(let i = 0; i < groups.length; i++){
+        if(groups[i].getElementsByTagName('name')[0].innerHTML == group){
+            let students = groups[i].getElementsByTagName('student');
+            for(let j = 0; j < students.length; j++){
+                let option = document.createElement('option');
+                let name = students[j].getElementsByTagName('name')[0].innerHTML;
+                option.innerHTML = name;
+                option.value = name;
+                nameSelect.append(option);
+            }
+            break;
+        }
+    }
+} 
+
+function setFacultet(group){
+    for(let i = 0; i < groups.length; i++){
+        if(groups[i].getElementsByTagName('name')[0].innerHTML == group){
+            facultetInput.value = groups[i].getElementsByTagName('facultet')[0].innerHTML;
+            break;
+        }
+    }
+}
+
+function setCource(group){
+    for(let i = 0; i < groups.length; i++){
+        if(groups[i].getElementsByTagName('name')[0].innerHTML == group){
+            courceInput.value = groups[i].getElementsByTagName('cource')[0].innerHTML;
+            break;
+        }
+    }
+}
+
+function fillSubjects(group){
+    table.querySelectorAll('.row:not(.header)').forEach(elem => {
+        elem.remove();
+    });
+    for(let i = 0; i < groups.length; i++){
+        if(groups[i].getElementsByTagName('name')[0].innerHTML == group){
+            let subjects = groups[i].getElementsByTagName('subject');
+            for(let i = 0; i < subjects.length; i++){
+                let row = document.createElement('div');
+                row.classList.add('row');
+                let cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.innerHTML = subjects[i].getElementsByTagName('name')[0].innerHTML;
+                row.append(cell);
+
+                for(let j = 0; j < 3; j++){
+                    let radio = document.createElement('div');
+                    radio.classList.add('cell');
+
+                    let input = document.createElement('input');
+                    input.setAttribute('type', 'radio');
+                    input.classList.add('mark');
+                    input.value = j;
+                    input.setAttribute('name', cell.innerHTML);
+                    radio.append(input);
+                    row.append(radio);
+                }
+
+
+                table.append(row);
+            }
+            break;
+        }
+    }
+    updateListeners();
+}
+
+function fillMarks(group, name){
+    for(let i = 0; i < groups.length; i++){
+        if(groups[i].getElementsByTagName('name')[0].innerHTML == group){
+
+            let students = groups[i].getElementsByTagName('student');
+
+            for(let j = 0; j < students.length; j++){
+                if(students[j].firstElementChild.innerHTML == name){
+                    let marks = students[j].getElementsByTagName('mark');
+                    let cells = table.querySelectorAll('.row:not(.header)');
+                    for(let z = 0; z < marks.length; z++){
+                        cells[z].querySelectorAll('input')[Number(marks[z].innerHTML)].checked = true;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fillGroups();
+groupSelect.addEventListener('change', (event) => {
+    fillNames(groupSelect.value);
+    setFacultet(groupSelect.value);
+    setCource(groupSelect.value);
+    fillSubjects(groupSelect.value);
+    fillMarks(groupSelect.value, nameSelect.value);
 })
 
-clearButton.addEventListener('click', (event) => {
-    setCookie('count', 0);
-    countText.innerHTML = "Вы ещё не были на этой странице";
+nameSelect.addEventListener('change', (event) => {
+    fillMarks(groupSelect.value, nameSelect.value);
 })
-exitButton.addEventListener('click', (event) => {
-    welcomeText.innerHTML = "Зарегистрируйтесь";
-    setCookie('name',"");
-})
+
+document.querySelector('#buttonBall').addEventListener('click',calculateAverage);
+
+document.querySelector('#clear').addEventListener('click',()=>{
+    document.querySelector("#resSpan").innerHTML = "" ;
+});
+
+
+function calculateNumbers(){
+    let zero = 0;
+    let one = 0;
+    let two = 0;
+    document.querySelectorAll('.mark').forEach(function(mark){
+        if(mark.value == 0){
+            zero++;
+        }
+        if(mark.value == 1){
+            one++;
+        }
+        if(mark.value == 2){
+            two++;
+        }
+    })
+    document.querySelector('#markZero').innerHTML = "Нулей: " + zero;
+    document.querySelector('#markOne').innerHTML = "Единиц: " + one;
+    document.querySelector('#markTwo').innerHTML = "Двоек: " + two;
+}
+calculateNumbers();
